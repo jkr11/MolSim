72486dea22be6e41338cf0c2994039e4fef8fffe
Maximilian Böhmichen <max@boehmichen.de>
2024-10-23 19:53:00 +0200

    added DEBUG_PRINT to all irrelevant prints, cleaned up progress print of simulation

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <filesystem>
 
 #include "calc/Verlet.h"
 #include "forces/gravity.h"
@@ -17,9 +18,16 @@
 
 void plotParticles(int iteration, outputWriter::VTKWriter& vtkWriter, ParticleContainer& particle_container);
 
+constexpr int output_interval = 10000; // seems to be a decent rate, can be changed to arbitrary numbers
 constexpr double start_time = 0;
-constexpr double end_time = 100;
-constexpr double delta_t = 0.014;
+double t_end = 100;
+double delta_t = 0.014;
+
+const std::string output = "output/";
+std::string output_directory;
+
+const std::string red = "\033[31m";
+const std::string reset = "\033[0m";
 
 // TODO: what data structure to pick?
 std::list<Particle> particles;
@@ -28,13 +36,38 @@ Gravity gravity;
 
 int main(int argc, char* argsv[]) {
   std::cout << "Hello from MolSim for PSE!" << std::endl;
-  if (argc != 2) {
-    std::cout << "Erroneous programme call! " << std::endl;
-    std::cout << "./molsym filename" << std::endl;
+
+  // safety checks
+  if (argc < 2) {
+    std::cout << red << "Erroneous programme call! " << reset << std::endl;
+    std::cout << "./molsym <filename> [<t_end>] [<delta_t>]" << std::endl;
+    exit(1);
+  }
+
+  // prepare output directory
+  const std::string input_file = argsv[1];
+  output_directory = output + input_file;
+  const std::filesystem::path output_directory_path = output_directory;
+
+  if (!std::filesystem::exists(input_file)) {
+    std::cout << red << "Input File '" << argsv[1] << "' does not exist" << reset << std::endl;
+    exit(1);
+  }
+
+  if (!is_directory(output_directory_path)) {
+    create_directories(output_directory_path);
+    std::cout << "Created directory for output: " << output_directory_path << std::endl;
+  }
+
+  // further arguments
+  if (argc == 4) {
+    t_end = std::stod(argsv[2]);
+    delta_t = std::stod(argsv[3]);
   }
 
   FileReader fileReader;
   fileReader.readFile(particles, argsv[1]);
+
 
   ParticleContainer particle_container(particles);
   VerletIntegrator verlet_integrator(gravity, delta_t);
@@ -45,18 +78,35 @@ int main(int argc, char* argsv[]) {
   int iteration = 0;
 
   // for this loop, we assume: current x, current f and current v are known
-  while (current_time < end_time) {
+  while (current_time < t_end) {
     verlet_integrator.step(particle_container);
     iteration++;
     if (iteration % 50 == 0) {
       plotParticles(iteration, writer, particle_container);
     }
-    std::cout << "Iteration " << iteration << " finished." << std::endl;
+
+    if (iteration % output_interval == 0) {
+      // clean up print if DEBUG is enabled
+      #ifdef DEBUG
+      std::cout << "Iteration " << iteration << " finished." << std::endl;
+      #else
+      const double completion_percentage = 100 * current_time / t_end;
+      const std::string output_string = "\r[" + std::to_string(completion_percentage) + " %]: "
+        + std::to_string(iteration) + " iterations finished";
+      std::cout << output_string << std::flush;
+      #endif
+    }
+
 
     current_time += delta_t;
   }
 
+  std::cout << std::endl;
   std::cout << "output written. Terminating..." << std::endl;
+  #ifdef DEBUG
+    std::cout << "DEBUG WAS ENABLED" << std::endl;
+  #endif
+
   return 0;
 }
 
@@ -67,5 +117,5 @@ void plotParticles(const int iteration, outputWriter::VTKWriter& vtkWriter, Part
     vtkWriter.plotParticle(p);
   }
 
-  vtkWriter.writeFile("MD_vtk", iteration);
+  vtkWriter.writeFile(output_directory + "/MD_vtk", iteration);
 }
