7174e428ba9d878278f80c29d1bffe0a2ca42d27
Maximilian Böhmichen <34962223+MaximilianBoehmichen@users.noreply.github.com>
2024-10-24 17:45:03 +0200

    Change outputs for prettier printing (#2)
    * Added CMAKE_BUILD_TYPE "Debug" for full console output, use default for clean output
    
    * added DEBUG_PRINT to all irrelevant prints, cleaned up progress print of simulation
    
    * changed IO to optarg, still unsre about output directory
    
    ---------
    
    Co-authored-by: jkr11 <113939131+jkr11@users.noreply.github.com>

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -4,37 +4,86 @@
 
 
 #include <defs/Particle.h>
-#include <array>
 #include <iostream>
 #include <list>
 #include <vector>
+#include <filesystem>
+#include <getopt.h>
 
 #include "calc/Verlet.h"
 #include "forces/gravity.h"
 #include "outputWriter/VTKWriter.h"
 
-/**** forward declaration of the calculation functions ****/
+constexpr std::string USAGE = "Usage: ./MolSim -f <filename> [-t <t_end>] [-d <delta_t>]";
 
+/**** forward declaration of the calculation functions ****/
 void plotParticles(int iteration, outputWriter::VTKWriter& vtkWriter, ParticleContainer& particle_container);
 
+constexpr int output_interval = 10000; // seems to be a decent rate, can be changed to arbitrary numbers
 constexpr double start_time = 0;
-constexpr double end_time = 100;
-constexpr double delta_t = 0.014;
+double t_end = 100;
+double delta_t = 0.014;
+
+const std::string output = "output/";
+std::string output_directory = "./output/";
+
+const std::string red = "\033[31m";
+const std::string reset = "\033[0m";
 
 // TODO: what data structure to pick?
 std::list<Particle> particles;
 
 Gravity gravity;
 
-int main(int argc, char* argsv[]) {
+int main(const int argc, char* argsv[]) {
   std::cout << "Hello from MolSim for PSE!" << std::endl;
-  if (argc != 2) {
-    std::cout << "Erroneous programme call! " << std::endl;
-    std::cout << "./molsym filename" << std::endl;
+  std::string input_file;
+  int opt;
+  while ((opt = getopt(argc, argsv, "f:t:d:")) != -1) {
+    switch (opt) {
+    case'f':
+      input_file = optarg;
+      break;
+    case 't':
+      t_end = std::stod(optarg);
+      break;
+    case 'd':
+      delta_t = std::stod(optarg);
+      break;
+    default:
+      std::cout << USAGE << std::endl;
+      exit(EXIT_FAILURE);
+    }
   }
 
+  if (input_file.empty()) {
+    std::cout << "Erroneous programme call, input_file empty!" << std::endl;
+    std::cout << USAGE << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  // so we cant really do that incase the input is a directory
+  // output_directory += input_file;
+  const std::filesystem::path output_directory_path = output_directory;
+
+  if (!std::filesystem::exists(input_file)) {
+    std::cout << red << "Input File '" << input_file << "' does not exist" << reset << std::endl;
+    exit(1);
+  }
+
+  // TODO: fix make this project specific, else just pass in the output path
+  if (!is_directory(output_directory_path)) {
+    create_directories(output_directory_path);
+    std::cout << "Created directory for output: " << output_directory_path << std::endl;
+  }
+
+  // further arguments
+  //check here if defaults have been used, optional type is c++17 sadly, so double copy??
+  std::cout << "t_end: " << t_end << ", delta_t: " << delta_t << std::endl;
+
   FileReader fileReader;
-  fileReader.readFile(particles, argsv[1]);
+  fileReader.readFile(particles,  input_file.c_str());
+
 
   ParticleContainer particle_container(particles);
   VerletIntegrator verlet_integrator(gravity, delta_t);
@@ -45,18 +94,35 @@ int main(int argc, char* argsv[]) {
   int iteration = 0;
 
   // for this loop, we assume: current x, current f and current v are known
-  while (current_time < end_time) {
+  while (current_time < t_end) {
     verlet_integrator.step(particle_container);
     iteration++;
     if (iteration % 50 == 0) {
       plotParticles(iteration, writer, particle_container);
     }
-    std::cout << "Iteration " << iteration << " finished." << std::endl;
+
+    if (iteration % output_interval == 0) {
+      // clean up print if DEBUG is enabled
+#ifdef DEBUG
+      std::cout << "Iteration " << iteration << " finished." << std::endl;
+#else
+      const double completion_percentage = 100 * current_time / t_end;
+      const std::string output_string = "\r[" + std::to_string(completion_percentage) + " %]: "
+        + std::to_string(iteration) + " iterations finished";
+      std::cout << output_string << std::flush;
+#endif
+    }
+
 
     current_time += delta_t;
   }
 
+  std::cout << std::endl;
   std::cout << "output written. Terminating..." << std::endl;
+#ifdef DEBUG
+  std::cout << "DEBUG WAS ENABLED" << std::endl;
+#endif
+
   return 0;
 }
 
@@ -67,5 +133,5 @@ void plotParticles(const int iteration, outputWriter::VTKWriter& vtkWriter, Part
     vtkWriter.plotParticle(p);
   }
 
-  vtkWriter.writeFile("MD_vtk", iteration);
+  vtkWriter.writeFile(output_directory + "/MD_vtk", iteration);
 }
---
+++ src/debug/debug_print.h
@@ -0,0 +1,17 @@
+//
+// Created by maximilian on 23.10.24.
+//
+#pragma once
+
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <iostream>
+
+#ifdef DEBUG
+#define DEBUG_PRINT(msg) std::cout<< "\t\033[38;2;255;165;0m " << msg << "\033[0m" << std::endl
+#else
+#define DEBUG_PRINT(msg)
+#endif
+
+#endif //DEBUG_H
