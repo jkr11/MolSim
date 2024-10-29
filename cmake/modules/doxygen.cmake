94c2c562418c937bcb07546d40313831d64f923b
Michael Carnuth <115880829+Michael-Carnuth@users.noreply.github.com>
2024-10-29 16:07:20 +0100

    Added cmake file for doxygen

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,23 @@
-# make doc_doxygen optional if someone does not have / like doxygen
+# option to disable doxygen
+option(ENABLE_DOXYGEN "Enable doxygen" ON)
 
-# TODO: create CMake build option for the target.
+if (NOT ENABLE_DOXYGEN)
+    message("Doxygen is disabled")
+    return()
+endif ()
 
-# TODO: Add a custom target for building the documentation.
+# find doxygen install
+find_package(Doxygen)
+
+if (NOT DOXYGEN_FOUND)
+    message("No doxygen install found")
+    return()
+endif ()
+
+# custom doc_doxygen target
+add_custom_target(doc_doxygen
+        COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile
+        VERBATIM
+)
+
+set_target_properties(doc_doxygen PROPERTIES EXCLUDE_FROM_ALL TRUE)
