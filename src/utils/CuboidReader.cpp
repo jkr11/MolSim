4f94f8f4fc46e8e6ffd542c10c540882164fe04e
jkr11 <113939131+jkr11@users.noreply.github.com>
2024-11-01 22:03:52 +0100

    allow for comments, fix constant assignment and average velocity

--- src/utils/CuboidReader.cpp
+++ src/utils/CuboidReader.cpp
@@ -12,10 +12,10 @@
 void CuboidReader::readCuboidFile(ParticleContainer& particle_container,
                                   std::string& fileName) {
   std::ifstream inputfile(fileName);
-  dvec3 corner{};
-  dvec3 velocity{};
-  std::array<int, 3> dimensions{};
   if (inputfile.is_open()) {
+    dvec3 corner{};
+    dvec3 velocity{};
+    std::array<int, 3> dimensions{};
     int ncubes;
     int type;
     double h;
@@ -25,9 +25,15 @@ void CuboidReader::readCuboidFile(ParticleContainer& particle_container,
     double sigma;
     std::string line;
     std::getline(inputfile, line);
+    while (line.empty() or line[0] == '#') {
+      getline(inputfile, line);
+      std::cout << "Read line: " + line << std::endl;
+    }
+    //sstd::getline(inputfile, line);
     std::istringstream numstream(line);
     numstream >> ncubes;
-    std::cout << "Reading " << ncubes << " cuboids from file " << fileName << std::endl;
+    std::cout << "Reading " << ncubes << " cuboids from file " << fileName
+              << std::endl;
     for (int i = 0; i < ncubes; i++) {
       std::getline(inputfile, line);
       std::istringstream linestream(line);
@@ -52,7 +58,8 @@ void CuboidReader::readCuboidFile(ParticleContainer& particle_container,
       std::cout << "epsilon: " << epsilon << std::endl;
       linestream >> sigma;
       std::cout << "sigma: " << sigma << std::endl;
-      CuboidGenerator cg(corner, dimensions, h, mass, velocity, mv, epsilon, sigma, type);
+      CuboidGenerator cg(corner, dimensions, h, mass, velocity, mv, epsilon,
+                         sigma, type);
       cg.generate(particle_container);
       std::cout << particle_container.size() << std::endl;
     }
