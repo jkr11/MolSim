bd3eccce6508ce8247ced00838fbdfdf6a97af54
Michael Carnuth <115880829+Michael-Carnuth@users.noreply.github.com>
2024-11-06 20:38:55 +0100

    Created abstract fileReader class to unify file input

--- src/io/file/FileReader.h
+++ src/io/file/FileReader.h
@@ -6,6 +6,6 @@
 #include "defs/Particle.h"
 
 class FileReader {
- public:
-  static void read(std::vector<Particle>& particles, const std::string& filepath);
+public:
+  static void read(std::vector<Particle> &particles, const std::string &filepath);
 };
