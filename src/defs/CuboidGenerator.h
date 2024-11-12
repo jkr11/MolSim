84b206d944108a51e5bfb38a0257579a002806bf
jkr11 <113939131+jkr11@users.noreply.github.com>
2024-11-12 22:26:43 +0100

    weird formatting happened

--- src/defs/CuboidGenerator.h
+++ src/defs/CuboidGenerator.h
@@ -7,9 +7,8 @@
 #include "ParticleGenerator.h"
 
 /**
- * @brief
- * Generates Particles to a container in the shape of a cuboid [dim1, dim2,
- * dim3]
+ * @brief Generates Particles to a container in the shape of a cuboid [dim1,
+ * dim2, dim3]
  */
 class CuboidGenerator final : public ParticleGenerator {
  private:
