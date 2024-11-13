00031d406d211cc321a94b3d74731fdb9ce49bfc
jkr11 <113939131+jkr11@users.noreply.github.com>
2024-11-13 19:23:34 +0100

    using -O3 -ffast-math, possible different iterator in integrator

--- src/calc/VerletIntegrator.cpp
+++ src/calc/VerletIntegrator.cpp
@@ -20,11 +20,30 @@ void VerletIntegrator::step(ParticleContainer& particle_container) {
   });
 
   // update force
+
   particle_container.pairIterator([this](Particle& p1, Particle& p2) {
     const dvec3 g12 = force.directionalForce(p1, p2);
     p1.setF(p1.getF() + g12);  // F_i = \sum_j F_ij
     p2.setF(p2.getF() - g12);  // g12 = -g21
   });
+
+
+  // this would save around more
+  /*
+  std::vector<Particle> particles = particle_container.getParticles();
+  for (int i = 0; i < particle_container.size(); i++) {;
+    dvec3 acc = particles[i].getF();
+    for (int j = i+1; j < particle_container.size(); j++) {
+      const dvec3 g12 = force.directionalForce(particles[i],particles[j]);
+      acc = acc + g12;
+      const dvec3 p2F = particles[j].getF() - g12;
+      particles[j].setF(p2F);
+    }
+    particles[i].setF(acc);
+  }
+  particle_container.setParticles(particles);
+  */
+
   // Now we use F_t and F_{t-1} to calculate the current velocity
   particle_container.single_iterator([this](Particle& p) {
     const dvec3 new_v =
