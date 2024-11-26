ca8c59d3c744d7a33ca1e0cbce791332ac985830
jkr11 <113939131+jkr11@users.noreply.github.com>
2024-11-26 15:10:21 +0100

    first results looking good

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -6,36 +6,65 @@ import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
 executable = os.path.join(script_dir, "../build/src/MolSim")
 input_dir = os.path.join(script_dir, "../input")
-input_file_template = "task32_{}k.xml"
+input_file_template_lc = "task32_{}k.xml"
 input_file_template_ds = "task32_{}k_ds.xml"
-a_values = [1, 2]
-execution_times = []
+a_values = [1, 2, 4, 8]
+execution_times_ds = []
+execution_times_lc = []
 
 if not os.path.exists(executable):
-    raise FileNotFoundError(f"Executable not found at {executable}")
+  raise FileNotFoundError(f"Executable not found at {executable}")
+
+"""
+for a in a_values:
+  print(f"Running for a = {a}...")
+  input_file = os.path.join(input_dir, input_file_template_ds.format(a))
+  if not os.path.exists(input_file):
+    raise FileNotFoundError(f"Input file not found at {input_file}")
+
+  start_time = time.perf_counter()
+  try:
+    subprocess.run([executable, "-f", input_file, str(a)], check=True)
+  except subprocess.CalledProcessError as e:
+    print(f"Error while running the executable for a = {a}: {e}")
+    execution_times_ds.append(None)
+    continue
+  end_time = time.perf_counter()
+
+  elapsed_time = end_time - start_time
+  execution_times_ds.append(elapsed_time)
+  print(f"Execution time for a = {a}: {elapsed_time:.2f} seconds")
+"""
 
 for a in a_values:
-    print(f"Running for a = {a}...")
-    input_file = os.path.join(input_dir, input_file_template_ds.format(a))
-    if not os.path.exists(input_file):
-        raise FileNotFoundError(f"Input file not found at {input_file}")
+  print(f"Running for a = {a}...")
+  input_file = os.path.join(input_dir, input_file_template_lc.format(a))
+  if not os.path.exists(input_file):
+    raise FileNotFoundError(f"Input file not found at {input_file}")
 
-    start_time = time.perf_counter()  # Start timing
-    try:
-        subprocess.run([executable, "-f", input_file, str(a)], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Error while running the executable for a = {a}: {e}")
-        execution_times.append(None)
-        continue
-    end_time = time.perf_counter()
+  start_time = time.perf_counter()
+  try:
+    subprocess.run([executable, "-f", input_file, str(a)], check=True)
+  except subprocess.CalledProcessError as e:
+    print(f"Error while running the executable for a = {a}: {e}")
+    execution_times_lc.append(None)
+    continue
+  end_time = time.perf_counter()
 
-    elapsed_time = end_time - start_time
-    execution_times.append(elapsed_time)
-    print(f"Execution time for a = {a}: {elapsed_time:.2f} seconds")
+  elapsed_time = end_time - start_time
+  execution_times_lc.append(elapsed_time)
+  print(f"Execution time for a = {a}: {elapsed_time:.2f} seconds")
+
+execution_times_ds = [14.52, 50.81, 192.51, 771.44]
 
 plt.figure(figsize=(8, 6))
-plt.plot(a_values*1000, execution_times, marker='o', label="Execution Time")
-plt.title("Execution Time vs number of particles")
+plt.plot([a * 1000 for a in a_values],
+         execution_times_ds,
+         execution_times_lc,
+         marker='o',
+         label="Execution Time")
+plt.title(
+    "Execution Time vs number of particles running on Intel i7 13700H, 32Gb")
 plt.xlabel("Particles")
 plt.ylabel("Execution Time (seconds)")
 plt.grid(True)
@@ -46,4 +75,3 @@ output_plot = os.path.join(script_dir, "execution_times.png")
 plt.savefig(output_plot)
 print(f"Plot saved to {output_plot}")
 plt.show()
-
