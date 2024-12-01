8894a6b710a7a653c264059b35e9dd175c4364ca
jkr11 <113939131+jkr11@users.noreply.github.com>
2024-12-01 15:22:32 +0100

    remove 0 checks, cannot happen

--- src/forces/LennardJones.cpp
+++ src/forces/LennardJones.cpp
@@ -8,9 +8,6 @@
 dvec3 LennardJones::directionalForce(Particle& p1, Particle& p2) const {
   const dvec3 rv = p2.getX() - p1.getX();
   const double r = ArrayUtils::L2Norm(rv);
-  if (r == 0) return {0, 0, 0};
-  // const double sigma = (p1.getSigma() + p2.getSigma()) / 2.0;
-  // const double epsilon = std::sqrt(p1.getEpsilon() * p2.getEpsilon());
   constexpr double sigma = 1.0;
   constexpr double epsilon = 5.0;
   const double sr = sigma / r;
@@ -21,7 +18,7 @@ dvec3 LennardJones::directionalForce(Particle& p1, Particle& p2) const {
   return force_magnitude * rv;
 }
 
-double LennardJones::simpleForce(Particle& p, double distance) {
+double LennardJones::simpleForce(const Particle& p, const double distance) {
   if (distance <= 0) return 0;
   const double sr = p.getSigma() / distance;
   const double sr6 = std::pow(sr, 6);
