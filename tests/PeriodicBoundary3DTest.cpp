9b1abf3d289b31ada067cdd69fb0a308257b4896
Maximilian Böhmichen <max@boehmichen.de>
2025-01-22 14:33:49 +0100

    test for 3d !x y !z periodic

--- tests/PeriodicBoundary3DTest.cpp
+++ tests/PeriodicBoundary3DTest.cpp
@@ -213,3 +213,185 @@ TEST(PeriodicBoundary3D, warpingX) {
       container.reflective_warp_around_testing({3, -1, 3}, xhigh);
   EXPECT_EQ(is_adjacent, false) << "ae";
 }
+
+/**
+ * tests warping in 3D for only y periodic
+ * and only the most interesting cells, because else it would be too much
+ */
+TEST(PeriodicBoundary3D, warpingY) {
+  const LinkedCellsContainer container(
+      {.domain = {3, 3, 3},
+       .cutoff_radius = 1,
+       .boundary_config = {
+           LinkedCellsConfig::BoundaryType::Outflow,
+           LinkedCellsConfig::BoundaryType::Outflow,
+           LinkedCellsConfig::BoundaryType::Periodic,
+           LinkedCellsConfig::BoundaryType::Periodic,
+           LinkedCellsConfig::BoundaryType::Outflow,
+           LinkedCellsConfig::BoundaryType::Outflow,
+       }});
+  bool is_adjacent;
+  ivec3 new_coordinates;
+  dvec3 offset;
+
+  // row 3
+  // ea
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({-1, 3, 3}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "ea";
+
+  // eb
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({0, 3, 3}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "eb";
+
+  // ec
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({1, 3, 3}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "ec";
+
+  // ed
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({2, 3, 3}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "ed";
+
+  // ee
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({3, 3, 3}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "ee";
+
+  //----------------------------------------//
+  // row2
+  // da
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({-1, 3, 2}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "da";
+
+  // db
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({0, 3, 2}, yhigh);
+  constexpr dvec3 expected_offset11 = {0, 3, 0};
+  EXPECT_EQ(is_adjacent, true) << "db";
+  EXPECT_IVEC3_EQ(new_coordinates, {0, 0, 2});
+  DVEC3_NEAR(offset, expected_offset11, "Offset wrong", 1e-5);
+
+  // dc
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({1, 3, 2}, yhigh);
+  constexpr dvec3 expected_offset12 = {0, 3, 0};
+  EXPECT_EQ(is_adjacent, true) << "dc";
+  EXPECT_IVEC3_EQ(new_coordinates, {1, 0, 2});
+  DVEC3_NEAR(offset, expected_offset12, "Offset wrong", 1e-5);
+
+  // dd
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({2, 3, 2}, yhigh);
+  constexpr dvec3 expected_offset13 = {0, 3, 0};
+  EXPECT_EQ(is_adjacent, true) << "dd";
+  EXPECT_IVEC3_EQ(new_coordinates, {2, 0, 2});
+  DVEC3_NEAR(offset, expected_offset13, "Offset wrong", 1e-5);
+
+  // de
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({3, 3, 2}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "de";
+
+  //----------------------------------------//
+  // row1
+  // ca
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({-1, 3, 1}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "ca";
+
+  // cb
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({0, 3, 1}, yhigh);
+  constexpr dvec3 expected_offset21 = {0, 3, 0};
+  EXPECT_EQ(is_adjacent, true) << "cb";
+  EXPECT_IVEC3_EQ(new_coordinates, {0, 0, 1});
+  DVEC3_NEAR(offset, expected_offset21, "Offset wrong", 1e-5);
+
+  // cc
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({1, 3, 1}, yhigh);
+  constexpr dvec3 expected_offset22 = {0, 3, 0};
+  EXPECT_EQ(is_adjacent, true) << "cc";
+  EXPECT_IVEC3_EQ(new_coordinates, {1, 0, 1});
+  DVEC3_NEAR(offset, expected_offset22, "Offset wrong", 1e-5);
+
+  // cd
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({2, 3, 1}, yhigh);
+  constexpr dvec3 expected_offset23 = {0, 3, 0};
+  EXPECT_EQ(is_adjacent, true) << "cd";
+  EXPECT_IVEC3_EQ(new_coordinates, {2, 0, 1});
+  DVEC3_NEAR(offset, expected_offset23, "Offset wrong", 1e-5);
+
+  // ce
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({3, 3, 1}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "ce";
+
+  //----------------------------------------//
+  // row0
+  // ba
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({-1, 3, 0}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "ba";
+
+  // bb
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({0, 3, 0}, yhigh);
+  constexpr dvec3 expected_offset31 = {0, 3, 0};
+  EXPECT_EQ(is_adjacent, true) << "bb";
+  EXPECT_IVEC3_EQ(new_coordinates, {0, 0, 0});
+  DVEC3_NEAR(offset, expected_offset31, "Offset wrong", 1e-5);
+
+  // bc
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({1, 3, 0}, yhigh);
+  constexpr dvec3 expected_offset32 = {0, 3, 0};
+  EXPECT_EQ(is_adjacent, true) << "bc";
+  EXPECT_IVEC3_EQ(new_coordinates, {1, 0, 0});
+  DVEC3_NEAR(offset, expected_offset32, "Offset wrong", 1e-5);
+
+  // bd
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({2, 3, 0}, yhigh);
+  constexpr dvec3 expected_offset33 = {0, 3, 0};
+  EXPECT_EQ(is_adjacent, true) << "bd";
+  EXPECT_IVEC3_EQ(new_coordinates, {2, 0, 0});
+  DVEC3_NEAR(offset, expected_offset33, "Offset wrong", 1e-5);
+
+  // be
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({3, 3, 0}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "be";
+
+  //----------------------------------------//
+  // row-1
+  // aa
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({-1, 3, -1}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "aa";
+
+  // ab
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({0, 3, -1}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "ab";
+
+  // ac
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({1, 3, -1}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "ac";
+
+  // ad
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({2, 3, -1}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "ad";
+
+  // ae
+  std::tie(is_adjacent, new_coordinates, offset) =
+      container.reflective_warp_around_testing({3, 3, -1}, yhigh);
+  EXPECT_EQ(is_adjacent, false) << "ae";
+}
